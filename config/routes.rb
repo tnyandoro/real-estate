5de0113d48a459b395ff1c184a4d0176b6b8cdfb
Tendai Nyandoro <tnyandoro@gmail.com>
2024-05-22 20:58:30 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  namespace :api do
+    resources :students
+    resources :courses
+    resources :enrollments
+    resources :certificates
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
