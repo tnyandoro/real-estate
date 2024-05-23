099c2d69da950e3a9e8656dee1efc503e197f732
Tendai Nyandoro <tnyandoro@gmail.com>
2024-05-23 12:36:59 +0200

    update the Certificates controller

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module ReazSchoolApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.encoding = "utf-8"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
